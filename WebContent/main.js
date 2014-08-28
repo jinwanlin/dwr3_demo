6b3ff6588a4daabf503fa54139b6636bc37a747b
jinwanlin123@gmail.com <jinwanlin123@gmail.com>
2014-08-29 00:15:23 +0800

    aaaa

--- WebContent/main.js
+++
@@ -1,9 +0,0 @@
-function init() {
-	dwr.engine.setActiveReverseAjax(true);
-}
-
-function send(){
-	var msg=dwr.util.getValue("msg");
-	chatManager.sendMessage(msg);
-	dwr.util.setValue("msg","");
-}
